0aa22fc0393c112dedb0de97a38368c79f2c1c36
Arsxen <archawat.sil@student.mahidol.ac.th>
2019-09-17 23:26:34 +0700

    Update Testing.java

--- src/Testing.java
+++ src/Testing.java
@@ -3,25 +3,17 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.*;
 
 
 public class Testing {
     public static void main(String[] args) throws IOException {
         ByteBuffer buffer = ByteBuffer.allocate(20);
-//        buffer.putInt(5);
-//        buffer.putInt(1);
-//        buffer.flip();
         RandomAccessFile random = new RandomAccessFile(new File("Testing/t.a"), "rw");
         FileChannel fc = random.getChannel();
-//        fc.write(buffer);
         fc.read(buffer);
         buffer.flip();
         System.out.println(buffer.getInt());
         System.out.println(buffer.getInt());
+
     }
 }
